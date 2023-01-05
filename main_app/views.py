c58781bf3d0f3c44545afc9fbc969a5799c5bffa
Anthony Siletti <asiletti@icloud.com>
2023-01-05 11:16:30 -0500

    stuff

--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,13 @@ from .models import Task
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
