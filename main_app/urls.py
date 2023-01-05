da343b85f9a6f85b0d10f94e6b8ccb98db375fbe
Anthony Siletti <asiletti@icloud.com>
2023-01-05 12:16:27 -0500

    fixed the nonsense

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
-    path('tasks/', views.TaskList.as_view(), name='tasks_index'),
+    path("", views.home, name="home"),
+    path("tasks/", views.TaskList.as_view(), name="tasks_index"),
+    path("tasks/<int:pk>/", views.TaskDetail.as_view(), name="tasks_detail"),
+    path("tasks/create/", views.TaskCreate.as_view(), name="tasks_create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,7 @@ from .models import Task
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -15,3 +16,12 @@ def home(request):
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = "__all__"
