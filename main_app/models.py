f157c6ba547116ae34c6a6b217db560347eb3df9
Matt <matt.atkins216@gmail.com>
2023-01-06 14:09:06 -0700

    text upon task creation

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+from twilio.rest import Client
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Create your models here.
 
@@ -23,3 +29,21 @@ class Task(models.Model):
 
     class Meta:
         ordering = ["-due_date"]
+ 
+    def save(self, *args, **kwargs):
+       
+            account_sid=env('TWILIO_ACCOUNT_SID')
+            auth_token=env('TWILIO_AUTH_TOKEN')
+            print(env('TWILIO_ACCOUNT_SID'))
+            print(env('TWILIO_AUTH_TOKEN'))
+            client = Client(account_sid, auth_token)
+
+            message = client.messages.create(
+                body=f'{ self.title } is due at { self.due_date }. This task is priority level: { self.priority } Here is your task description: { self.description }',
+                from_='+13393304638',
+                status_callback='http://postb.in/1234abcd',
+                to='+17204721204'
+                )
+
+            print(message.sid)
+            return super().save(*args, **kwargs)
